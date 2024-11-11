edaa62df7d1e72c72ce8bb28f45b876296b3a1e1
UYSALCORP <uysalcorp@gmail.com>
2024-11-11 20:16:09 +0300

    string start

---
+++ JAVASCRIPT/js-06-Strings/app.js
@@ -0,0 +1,18 @@
+console.log("Strings");
+//! STRINGS
+
+//! String tanımlama
+const str1 = "Clarusway"
+const str2 = `UYSALCORP`
+
+console.log(str2);
+console.log(typeof str2);
+
+// String constructor ile String oluşturma
+const str3 = new String("Way to reinvent yourself")
+console.log(typeof str3);
+
+//? Normalde primitive veri tiplerinin property veya metodları olmaz. Js otomatik olarak stringleri string objectlere dönüştürür.
+
+//! String Yapısı ve Indexleme
+let kelime = "Clarusway"
