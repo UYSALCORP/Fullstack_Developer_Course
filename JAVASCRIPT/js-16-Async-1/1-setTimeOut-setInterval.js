fb1b02074b1352c9f3f2299be0986f7793b36743
UYSALCORP <uysalcorp@gmail.com>
2024-12-09 20:51:07 +0300

    promise start

--- JAVASCRIPT/js-16-Async-1/1-setTimeOut-setInterval.js
+++ JAVASCRIPT/js-16-Async-1/1-setTimeOut-setInterval.js
@@ -18,3 +18,31 @@
 
 
 //* Asenkron (1 seferlik çalışan) setTimeOut
+
+// setTimeout(()=>{
+//     console.log("Platin Avcısı 26");
+// }, 3000) // 3 saniye sonra çalış. ms cinsinden çalışıyor
+
+// console.log("KadınaEmirVer54");
+
+// setTimeout(()=>{
+//     console.log("Bro Şlo Keke Mic Var Mı Mic?");
+// }, 2000) // 2 saniye sonra çalış
+
+//! Çalışma Sırası: 26-29-23
+
+//*Asenkron(setInterval, clearInterval) belli aralıklarla durdurulana kadar devam eder. mesela 1 dakikada bir döviz kurunu güncellemek için kullanılabilir
+//***************************** */
+
+//?setInterval non-blocking (kod burada takılı kalmaz alttaki kodlarda çalışır)
+
+let sayac = 0;
+//interval'ı çalıştırmak.
+const interval1 = setInterval(() => {
+    console.log(++sayac);
+    if (sayac==7){
+        // İnterval'ı durdurmak için 
+        clearInterval(interval1)
+    }
+}, 1000);
+
