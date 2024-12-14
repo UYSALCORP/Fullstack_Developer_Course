5a834e25f1125f4cf3704118bbe63acabe4f1452
UYSALCORP <uysalcorp@gmail.com>
2024-12-14 11:44:32 +0300

    node yapı

---
+++ JAVASCRIPT/js-18-Modules/3-node-es-modules/index.js
@@ -0,0 +1,11 @@
+import topla from "./hesapla";
+
+
+console.log(topla(5,4));
+
+// Klasörü node projesi yapmak için terminal'e : "npm init"
+
+// VITE
+// - npm create vite@latest
+// - npm install
+// - npm run dev
