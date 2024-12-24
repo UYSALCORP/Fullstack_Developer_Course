22ea8336fc1cd98e239ebee19c1ea0ebac23ba31
UYSALCORP <uysalcorp@gmail.com>
2024-12-24 23:00:11 +0300

    first react-lesson end

--- REACT/class-notes/first-app/src/App.js
+++ REACT/class-notes/first-app/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import helloreact from './assets/react.png'
 import './App.css';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <h1>React First-class</h1>
+      <p>Merhaba React!</p>
+      <img src={helloreact}className="hello-picture"></img>
     </div>
   );
 }
---
+++ REACT/class-notes/first-app/src/components/header.jsx
@@ -0,0 +1,11 @@
+import  React  from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <p> İlk component 🎸</p>
+        </div>
+    )
+}
+
+export default Header
