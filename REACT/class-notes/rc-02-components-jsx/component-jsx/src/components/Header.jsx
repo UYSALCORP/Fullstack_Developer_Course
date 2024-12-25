ef26c943793fa9bbd73726ebefc763a10a7e2a0e
UYSALCORP <uysalcorp@gmail.com>
2024-12-25 21:51:02 +0300

    header component

--- REACT/class-notes/rc-02-components-jsx/component-jsx/src/components/Header.jsx
+++ REACT/class-notes/rc-02-components-jsx/component-jsx/src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react"
+//* rafc(exportArrowFunction kısayol), rafce(arrowFunction kısayol)
 
+//! Arrow Function with default export (rafce)
 const Header = () =>{
+    const mesaj = "Say hi to React with Clarusway"
     return(
         <header>
         <h3>Yunus Emre Uysal - Frontend Developer with React</h3>
+        <p className="par">{mesaj}</p>
         </header>
     )
 }
