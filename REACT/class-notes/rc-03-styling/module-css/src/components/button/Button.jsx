c623367ddcd709d8b80328da0676ecb015d76b70
UYSALCORP <uysalcorp@gmail.com>
2024-12-26 21:59:40 +0300

    sassusing

--- REACT/class-notes/rc-03-styling/module-css/src/components/button/Button.jsx
+++ REACT/class-notes/rc-03-styling/module-css/src/components/button/Button.jsx
@@ -4,7 +4,7 @@ const Button = () => {
     return (
         <div className={ButtonStyle.wrapper}>
             {/* <button className="btn btn-blue">Info</button> */}
-            //! CSS MODULES
+            {/* //! CSS MODULES */}
             <button className={`${ButtonStyle.btn} ${ButtonStyle["btn-blue"]}`}>Info</button>
             <button className={`${ButtonStyle.btn} ${ButtonStyle["btn-yellow"]}`}>Back</button>
         </div>
