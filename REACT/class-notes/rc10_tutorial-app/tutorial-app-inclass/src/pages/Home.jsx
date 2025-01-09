bc3203e27b9073d21a672e47cf0e57cbf891ffc2
UYSALCORP <uysalcorp@gmail.com>
2025-01-09 21:00:34 +0300

    mola

--- REACT/class-notes/rc10_tutorial-app/tutorial-app-inclass/src/pages/Home.jsx
+++ REACT/class-notes/rc10_tutorial-app/tutorial-app-inclass/src/pages/Home.jsx
@@ -1,7 +1,16 @@
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
+import axios from "axios"
 
 const Home = () => {
+  const getTutorials = () => {
+    const URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
+    axios
+      .get(URL)
+      .then((res) => console.log(res.data))
+      .catch((error) => console.log(error));
+  };
+  getTutorials();
   return (
     <>
       <AddTutorial />
