12f2dcb6915072aab2c5bee62e457a28c0a28729
UYSALCORP <uysalcorp@gmail.com>
2025-01-09 20:49:28 +0300

    starter

--- REACT/class-notes/rc09_lifecycle-useEffect/src/components/User.jsx
+++ REACT/class-notes/rc09_lifecycle-useEffect/src/components/User.jsx
@@ -1,24 +1,48 @@
-import React from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const User = () => {
-    const getUser = async()=>{
-       const result =  await axios.get("https://randomuser.me/api/")
-       console.log(result)
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    dob: "",
+    picture: "",
+    location: "",
+    cell: "",
+  });
+  const getUser = async () => {
+    try {
+      const result = await axios.get("https://randomuser.me/api/");
+      setUserData(result.data.results[0]);
+    } catch (error) {
+      console.log(error);
     }
-    getUser()
+  };
+
+  //? componentDidMount
+  useEffect(() => {
+    const timerId = setInterval(getUser, 5000)
+    getUser();
+    //? componentWillUnmount
+    return () => {
+        console.log("Timer iptal edildi.")
+        clearInterval(timerId)
+    }
+  }, []); //? Dependency array
+
   return (
     <div>
-      <img src="" alt="" />
+      <img className="rounded-circle m-4" src={userData.picture.large} alt="" />
       <p>Hi, My name is: </p>
-      <h2>username</h2>
-      <h4>email:</h4>
-      <h5>birth:</h5>
-      <h5>city:</h5>
-      <h5>tel no:</h5>
-      <button className='btn btn-danger'>New User</button>
+      {/* //* İlk renderda state değeri initial değer olacağı için hata almamak adına ya state'e initial değer vermeliyiz ya da optional chaning "?" kontrolü yapmalıyız! */}
+      <h2>{userData?.name.first + " " + userData?.name.last}</h2>
+      <h4>email:{userData.email}</h4>
+      <h5>birth:{userData.dob.date}</h5>
+      <h5>city:{userData.location.city}</h5>
+      <h5>tel no:{userData.cell}</h5>
+      <button onClick={getUser} className="btn btn-danger">New User</button>
     </div>
-  )
-}
+  );
+};
 
-export default User
+export default User;
---
+++ REACT/class-notes/rc10_tutorial-app/tutorial-app-inclass/src/components/AddTutorial.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+const AddTutorial = () => {
+  const [title, setTitle] = useState("")
+  const [desc, setDesc] = useState("")
+
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log(title, desc)
+    // postTutorial(title, desc)
+  }
+
+  return (
+    <div className="container text-center mt-4">
+      <h1 className="display-6 text-danger">Add Your Tutorial</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            onChange={(e)=>setTitle(e.target.value)}
+            id="title"
+            placeholder="Enter your title"
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="desc" className="form-label">
+            Description
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            onChange={(e)=>setDesc(e.target.value)}
+            id="desc"
+            placeholder="Enter your Description"
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-danger mb-4">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddTutorial;
