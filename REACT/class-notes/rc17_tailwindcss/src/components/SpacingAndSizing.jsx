0b9c3620aa34fa554e81eea57d3e9f9df54b51f2
UYSALCORP <uysalcorp@gmail.com>
2025-01-22 21:19:59 +0300

    spacing and sizing

--- REACT/class-notes/rc17_tailwindcss/src/components/SpacingAndSizing.jsx
+++ REACT/class-notes/rc17_tailwindcss/src/components/SpacingAndSizing.jsx
@@ -11,9 +11,11 @@ const SpacingAndSizing = () => {
       </div>
       <div>
         <button className="bg-green-500 mx-4 px-4 py-2 border-[3px] border-black rounded-md">Gönder</button>
+        <button className='bg-green-500 px-4 py-4 border-[3px] border-l-green-900 rounded-[50%]'></button>
       </div>
       <div>
-        <button></button>
+        <button className="bg-amber-500 w-1/3 rounded-full py-2 mx-2">Kaydet</button>
+        <button className="bg-amber-500 w-1/2 max-w-[300px] rounded-full py-2 mx-2">Kaydet</button>
       </div>
     </div>
   )
