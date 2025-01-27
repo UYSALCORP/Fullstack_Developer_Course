8c3a9a633b7154b42cdacf3ca7d8d588c70509fd
UYSALCORP <uysalcorp@gmail.com>
2025-01-27 22:24:48 +0300

    Text field components

--- REACT/class-notes/rc20-MaterialUI/src/components/TextFieldComp.jsx
+++ REACT/class-notes/rc20-MaterialUI/src/components/TextFieldComp.jsx
@@ -1,9 +1,122 @@
-import React from 'react'
+import { Avatar, Box, Button, Container, FormControl, IconButton, Input, InputAdornment, InputLabel, MenuItem, OutlinedInput, TextField, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 const TextFieldComp = () => {
+  const [err, setErr] = useState(false);
+  const currencies = [
+    {
+      value: 'USD',
+      label: '$',
+    },
+    {
+      value: 'EUR',
+      label: '€',
+    },
+    {
+      value: 'BTC',
+      label: '฿',
+    },
+    {
+      value: 'JPY',
+      label: '¥',
+    },
+  ];
   return (
-    <div>TextFieldComp</div>
-  )
-}
+    <div>
+      <Container>
+        <Typography variant="h4" sx={{ textAlign: "center", mt: 4 }}>
+          TEXT-FIELD
+        </Typography>
+        <Box onClick={() => setErr(!err)} sx={{ mt: 4 }}>
+          <TextField
+            required
+            id="outlined-basic"
+            label="required"
+            variant="outlined"
+            defaultValue="E-mail giriniz."
+            // margin="dense" //! Ufak bir margin değeri verir.
+          />
 
-export default TextFieldComp
+          <TextField
+            id="outlined-helperText"
+            label="Helper text"
+            defaultValue="Default Value"
+            helperText={err && "Yanlış Girdiniz!"}
+            sx={{ ml: 4 }}
+          />
+
+          <TextField
+            error={err}
+            id="standard-error-helper-text"
+            label={err && "Error"}
+            defaultValue="Hello World"
+            helperText="Incorrect entry."
+            variant="standard"
+          />
+        </Box>
+
+        <Box sx={{mt:5}}>
+        <TextField
+          id="outlined-select-currency"
+          select
+          label="Select"
+          defaultValue="EUR"
+          helperText="Please select your currency"
+        >
+          {currencies.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+
+        <FormControl variant="standard">
+            <InputLabel htmlFor="input-with-icon-adornment">
+              With a start adornment
+            </InputLabel>
+            <Input
+              id="input-with-icon-adornment"
+              startAdornment={
+                <InputAdornment position="start">
+                  <AccountCircle />
+                </InputAdornment>
+              }
+            />
+          </FormControl>
+        </Box>
+
+        <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={err? "password" : "text" }
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password"
+                  onClick={()=> setErr(!err)}
+                  edge="end"
+                >
+                 {err ? <VisibilityOffIcon/> : <VisibilityIcon/>}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+      </Container>
+
+      <Button fullWidth variant="contained" color='error' sx={{gap:3, background: "gray"}}>
+        SUBMIT
+        <Avatar alt="Travis Howard" src="https://cdn.pixabay.com/photo/2021/08/11/11/15/man-6538205_1280.jpg" />
+      </Button>
+    </div>
+  );
+};
+
+export default TextFieldComp;
--- REACT/class-notes/rc20-MaterialUI/src/pages/Home.jsx
+++ REACT/class-notes/rc20-MaterialUI/src/pages/Home.jsx
@@ -1,21 +1,18 @@
-import React from 'react'
-import TypoButtons from '../components/TypoButtons'
-import CardGrid from '../components/CardGrid'
-import Navbar from '../components/Navbar'
-import TextFieldComp from '../components/TextFieldComp'
+import React from "react";
+import TypoButtons from "../components/TypoButtons";
+import CardGrid from "../components/CardGrid";
+import Navbar from "../components/Navbar";
+import TextFieldComp from "../components/TextFieldComp";
 
 const Home = () => {
   return (
     <>
-
-    <Navbar/>
-   <TypoButtons/>
-   <TextFieldComp/>
-<CardGrid/>
-
-
+      <Navbar />
+      {/* <TypoButtons /> */}
+      {/* <TextFieldComp /> */}
+      <CardGrid />
     </>
   );
-}
+};
 
-export default Home
+export default Home;
