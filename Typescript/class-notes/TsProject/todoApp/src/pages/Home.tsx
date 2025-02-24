6937f95ef5039cd5d7fc9c02630c0fe283398bef
UYSALCORP <uysalcorp@gmail.com>
2025-02-24 21:23:05 +0300

    header

---
+++ Typescript/class-notes/TsProject/todoApp/src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import AddTodo from "../components/AddTodo"
+import Header from "../components/Header"
+import TodoList from "../components/TodoList"
+
+
+const Home = () => {
+  return (
+    <div>
+        <Header/>
+        <AddTodo/>
+        <TodoList/>
+    </div>
+  )
+}
+
+export default Home
