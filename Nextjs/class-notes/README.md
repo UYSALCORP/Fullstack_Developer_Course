805fb7357fdfcd91bdd6f280c4e5a138d764ba3a
UYSALCORP <uysalcorp@gmail.com>
2025-02-27 20:41:47 +0300

    profile

--- Nextjs/class-notes/README.md
+++ Nextjs/class-notes/README.md
@@ -1,3 +1,14 @@
 # Nextjs Class Notes
 
 - List of Nextjs Class Notes as follow :
+
+
+# What is Next.js, and how is it different from React?
+# Answer:
+## Next.js is a React-based open-source framework that helps developers build server-side rendered React applications.
+
+# The key difference between React and Next.js is the way they handle routing. React uses client-side routing, meaning the page transitions are handled entirely on the client-side using JavaScript.
+
+# In contrast, Next.js provides server-side routing, which means that the server handles the routing and sends the pre-rendered pages to the client, resulting in faster page loads and better SEO.
+
+# Next.js also provides additional features like automatic code splitting, static site generation, and dynamic imports.
---
+++ Nextjs/class-notes/netflix-app/src/app/(private)/profile/page.jsx
@@ -0,0 +1,44 @@
+"use client"
+import { useAuthContext } from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const profileImages = [
+  "/images/default-blue.png",
+  "/images/default-red.png",
+  "/images/default-slate.png",
+  "/images/default-green.png",
+];
+
+const Profile = () => {
+  const { currentUser } = useAuthContext()
+  const router = useRouter()
+  return (
+    <div className="flex items-center justify-center pt-20">
+      <div className="flex flex-col">
+        <h1 className="text-3xl md:text-6xl text-white text-center">
+          Who's watching?
+        </h1>
+        <div className="flex flex-col md:flex-row items-center justify-center gap-8 mt-10">
+          {profileImages.map((image, index) => (
+            <div className="w-44 mx-auto cursor-pointer group">
+              <div onClick={()=>router.push("/movies")} className="w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden">
+                <img
+                  draggable={false}
+                  className="w-max h-max object-contain"
+                  src={image}
+                  alt=""
+                />
+              </div>
+              <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-white">
+                {index === 0 && currentUser ? currentUser.displayName:`Guest-${index+1}`}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
