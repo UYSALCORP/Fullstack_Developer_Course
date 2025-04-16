6c3cb85b7e1e812b8f8d82134aebb02be5371c98
UYSALCORP <uysalcorp@gmail.com>
2025-04-16 21:39:24 +0300

    feat: add department controller and routes

--- nodejs/class-notes/17-PersonelApi/src/controllers/department.js
+++ nodejs/class-notes/17-PersonelApi/src/controllers/department.js
@@ -3,3 +3,46 @@
     EXPRESS - Personnel API
 ------------------------------------------------------- */
 const Department = require("../models/department")
+
+module.exports = {
+    list: async (req, res) => {
+        const result = await getModelList(Department)
+        res.status(200).send({
+            error: false,
+            details: await getModelListDetails(Department),
+            result
+        })
+    },
+    create: async (req, res) => {
+        const result = await Department.create(req.body)
+        // 201 : Create
+        res.status(201).send({
+            error: false,
+            result
+        })
+    },
+    read: async (req, res) => {
+        const result = await Department.findOne({_id:req.params.id})
+        res.status(200).send({
+            error: false,
+            result
+        })
+    },
+    update: async (req, res) => {
+        const result = await Department.updateOne({_id:req.params.id}, req.body)
+        // 202 : Update
+        res.status(202).send({
+            error: false,
+            result
+        })
+    },
+    delete: async (req, res) => {
+        const result = await Department.deleteOne({_id:req.params.id})
+        // 204 : No Content - 404 : Not Found
+        res.status(result.deletedCount ? 204 : 404).send({
+            error: true,
+            message: "Data is not found! OR already deleted.",
+            result
+        })
+    }
+}
