ce600ef7f14e69c91d5720f95925b6bc5cb32017
UYSALCORP <uysalcorp@gmail.com>
2025-05-25 22:09:36 +0300

    update typescript readme

--- Typescript/README.md
+++ Typescript/README.md
@@ -1,15 +1,81 @@
-# Typescript Class Notes
+# Typescript Notes
 
-## Cross-Platform: JS'in çalıştırdığı bütün platformlarda çalışır.
+## Installation:
+- npm install -g typescript (Global'de kurulum)
+- npm install -D typescript (devDependencies'de package.json'a (proje'ye) kurma.)
 
-## Object Oriented Language: Classes, interfaces ve modules gibi güçlü özellikleri barındırır.
+## Version Control:
+- tsc -v
 
-## Static-Type-Checking: Static typing, tipler yürütme anında tanımlanır.
+## Run a Project:
+- tsc --init
+- After specified directions run "tsc" command. It compiles Typescript to Javascript. 
+- tsc --watch automaticly compile 
 
-## Optional Static Typing: JS'in dinamik yapısı yerine Opsiyonel static typing yazabilirsin.
+## tsconfig.json
+- "rootDir" -> Specify the root folder within your source files. (Ana klasör'ü belirt.) (Row 29 in tsconfig.json)
+- "outDir" -> Specify an output folder for all emitted files. */ (Çıkış dosyası) (Row 61 in tsconfig.json)
+- "target" -> Set the JavaScript language version for emitted JavaScript and include compatible library declarations. (Row 14)
+- "removeComments" -> Disable emitting comments (Derlendiğinde yorum satırlarını kaldırır.) (Row 62)
+- "noEmitOnError" -> Disable emitting files if any type checking errors are reported. (Hata varsa compile etmez.) (Row 72)
+ 
+## Cross-Platform:
+- JS'in çalıştırdığı bütün platformlarda çalışır.
+- JS kodlarına compile edilir. Yoksa browserlarda çalışmaz.
 
-## DOM Manipulation: JS gibi
+## Object Oriented Language:
+- Classes, interfaces, type security ve modules gibi güçlü özellikleri barındırır.
 
-## ES6 Features: JS'de bulunan özellikleri barındırır.
+## Static-Type-Checking:
+- Static typing, tipler yürütme anında tanımlanır.
 
-# Type Annotation (Dipnot): (:) kullanarak yapılır fe: let decimal : number = 6
+## Optional Static Typing:
+- JS'in dinamik yapısı yerine Opsiyonel static typing yazabilirsin.
+
+## DOM Manipulation:
+- JS gibi
+
+## ES6 Features:
+- JS'de bulunan özellikleri barındırır.
+- Arrow function, Destructuring vs.
+
+# Type Annotation (Dipnot):
+- (:) kullanarak yapılır fe: let decimal : number = 6
+- let(Declare) message(Name it) :(Annotate it) string(Data Type) = "Hello World!"(Initialise)
+
+# Data Types - Basic Types:
+- Boolean: Simple true/false value
+- Number: All numbers are either floating point values or BigIntegers
+- String: Textual data types
+
+# Data Types - Arrays:
+- Can be written in one of two ways;
+- 1- use the type of the elements followed by [] -> let list: number[] = [1,2,3];
+- 2- generic array type, Array<elementType> -> let list: Array<number> = [1,2,3];
+
+# Data Types - Tuples:
+- Element types are known, but need not be the same.
+- 1- Declare a tuple type: let x:[string, number];
+- 2- Initialize it: x = ["UYSAL", 20]; -> It's OK.
+- 3- Initialize it incorrectly: x = [10, "Yunus"]; -> Error.
+
+# Data Types - Enum:
+- Is way of gibing more friendly names to sets of numeric values. 
+- Enum begin numbering their members starting at 0.
+
+# Data Type - Any:
+- TS'nin sıkı tür denetimini devre dışı bırakır
+- Can be use for dynamic contents, user-entered values from third party libraries.
+- let arr: any[] =["Yunus", 24, true]; (Array'e tür tanımlaması yapmamış oluyoruz fakat ts erroru da almıyoruz.)
+
+# Pros (Avantajlar):
+- Strict Typing: Kesin tip tanımlaması. (Number, string?)
+- Structural Typing: Yapısal olarak tip kontrolü. (Bi nesne tanımlayıp, o nesneden başka nesneler üretmek gibi.)
+- Type Annotations: Tiplerin belirtilmesi. Öneri olarak sunulur, gözardı edebilirsin.
+- Type inference: Otomatik olarak tipi algılar. ("merhaba" yazdığında onun string olduğunu algılaması gibi.)
+
+# Cons (Dezavantajlar):
+- Not true static typing.
+- One more Javascript to learn
+- Adding extra step - transpiling (Derlenme zamanı, compile işlemi)
+- Bloated code (More lines of coding)
--- Typescript/class-notes/ts01/src/app.ts
+++ Typescript/class-notes/ts01/src/app.ts
@@ -1,3 +1,8 @@
+console.log("Merhaba TypeScript")
+// TypeScript, JavaScript'in bir üst kümesi olan ve statik tip denetimi sağlayan bir programlama dilidir.
+// TypeScript, JavaScript'in tüm özelliklerini destekler ve ek olarak tip güvenliği, sınıflar, arayüzler ve modüller gibi özellikler sunar.
+// TypeScript, JavaScript kodunu derleyerek çalışır ve bu sayede hataları erken aşamada yakalayabiliriz.
+
 let topla = 23+45
 
 // Toplama İşlemi ile sonucu gösterdik.
@@ -8,30 +13,38 @@ console.log(toplam(54, 26))
 let b= 5
 b = "Hello"
 
-//! TYPE ANNOTATION
+/* -------------------------------------------------------------------------- */
+/*                             //! TYPE ANNOTATION                            */
+/* -------------------------------------------------------------------------- */
 // ts'de type annotation ile kesin olarak tip belirtiyoruz
 let c : number = 45
 console.log(typeof c)
 c = "Hello"
 console.log(typeof c)
 
-//! ARRAY DATA TYPE
+/* -------------------------------------------------------------------------- */
+/*                             //! ARRAY DATA TYPE                            */
+/* -------------------------------------------------------------------------- */
 // bir türl tanımlaması yapıldıgında array hep o türde olmalı
 let CodeList: number[]=[26,54,34]
 console.log(CodeList)
 CodeList.push("Ali")
-//? Generic Array tanımlama
-let serialList: Array<number> = [34,34,34]
+/* ----------------------- //? Generic Array tanımlama ---------------------- */
+let serialList: Array<number> = [34,34,45,56,67]
 console.log(serialList)
 
-//! TUPLE 
+/* -------------------------------------------------------------------------- */
+/*                                  //! TUPLE                                 */
+/* -------------------------------------------------------------------------- */
 // Sabit uzunluktaki ve belirli türlerde elemanlar içeren dizi türü. Her eleman belirlenen veri tipine sahip olması gerekir.
 
 let tuple1:[number, string, string]
 tuple1= [34,"Yunus","Uysal"]
 console.log(tuple1)
 
-//! TUPLE ARRAYI
+/* -------------------------------------------------------------------------- */
+/*                              //! TUPLE ARRAYI                              */
+/* -------------------------------------------------------------------------- */
 let sinif : [number, string][]
 sinif = [[26,"Uysal"],[35,"Uysal"]]
 console.log(sinif)
@@ -42,12 +55,18 @@ let [brand, model] = car
 console.log(brand)
 
 //! Tuple içindeki değerlerin değişitirilmesini engellemek için readonly kullanabilirsiniz!
+// readonly ile tanımlanan bir tuple, elemanlarının değerleri değiştirilemez.
+// Ancak, elemanların türleri hala korunur.
 let settings:readonly[string, number]= ["Dark Mode",1]
 settings=["Light Mode", 2]
 settings[0]="Normal Mode"
 console.log(settings)
 
 //! ENUM
+// Enum, sabit değerlerin adlandırılmış bir kümesini tanımlamak için kullanılır.
+// Enum, genellikle bir grup ilişkili sabit değeri temsil etmek için kullanılır.
+// Enum, kodun okunabilirliğini artırır ve sabit değerleri daha anlamlı hale getirir.
+// Enum, JavaScript'te bir nesne olarak derlenir ve sabit değerler içerir.
 enum Color {
     Red, 
     green, 
@@ -78,12 +97,16 @@ let veri : unknown;
 veri="Nasılsın"
 console.log(typeof veri)
 
-// Tip kontrolü yapılmalı - yoksa unknown ile verilen bir değer işleme oskulmaz
+// Unknown, herhangi bir türdeki verileri temsil eder, ancak unknown türündeki bir değeri kullanmadan önce tip kontrolü yapmanız gerekir.
+// Bu, unknown türünün güvenliğini sağlar ve hataları önler.
+
+// Tip kontrolü 1.yöntem
 if(typeof veri == "string"){
     console.log(veri.toLowerCase())
 }
 // Tip kontrolü 2.yöntem
 console.log((veri as string).length)
+/* ------------------------------------ - ----------------------------------- */
 
 //! VOID 
 // Return kullanmazsın, herhangi bir değer döndermezsin
@@ -92,22 +115,29 @@ function selamla(isim:string):void{
     console.log(`Merhaba ${isim}`)
 }
 selamla("Uysal")
+/* ------------------------------------ - ----------------------------------- */
 
 //! UNION (Birleşim) JS'deki or yapısı
+// Birden fazla türü birleştirmek için kullanılır.
+// Bir değişkenin birden fazla türde değer alabileceğini belirtir.
 let a : number = 26
 // number veya string
 let s:number | string
 s=45
 s="Merhaba"
+/* ------------------------------------ - ----------------------------------- */
 
 //! TYPE ALIASES
+// Type Aliases, bir türü yeniden adlandırmak için kullanılır.
+// Bu, kodun okunabilirliğini artırır ve karmaşık türleri daha anlamlı hale getirir.
 // Kendimiz bir tip oluşturduk
 type name =string;
 let kullaniciAdi: name="Uysal"
 
 type kombine=number | string | boolean
 let sifre: kombine="Merhaba"
-
+/* ------------------------------------ - ----------------------------------- */
+// TypeScript'te string literal türleri, belirli bir dize değerini temsil eden özel türlerdir.
 //! String Literal
 type Pet="Dog"| "Cat"| "Bird"
 let myPet: Pet="Dog"
